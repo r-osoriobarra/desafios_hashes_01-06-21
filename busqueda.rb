62be5098e8930d29e8eaab0c29f6ded6a9747fa8
rodrigo <rob.developer@protonmail.com>
2021-06-02 18:42:35 -0400

    add: busqueda.rb

---
+++ busqueda.rb
@@ -0,0 +1,21 @@
+#Desafío 4
+
+ventas = {
+    Enero: 15000,
+    Febrero: 22000,
+    Marzo: 12000,
+    Abril: 17000,
+    Mayo: 81000,
+    Junio: 13000,
+    Julio: 21000,
+    Agosto: 41200,
+    Septiembre: 25000,
+    Octubre: 21500,
+    Noviembre: 91000,
+    Diciembre: 21000
+    }
+
+ARGV.each do |search|
+    month = ventas.invert[search.to_i]
+    pp month ? month : "no encontrado"
+end
